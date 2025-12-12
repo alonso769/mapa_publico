0a30bd68c3ef4894b83004c93cc8f57f3e9bc5c9
alonso769 <131563220+alonso769@users.noreply.github.com>
2025-12-12 14:59:56 -0500

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-  // CAMBIO NECESARIO PARA VERCEL
-  base: '/', 
+  // CAMBIO NECESARIO PARA GITHUB PAGES: Usa el nombre del repositorio
+  base: '/mapa_publico/', 
 
   build: {
     outDir: 'docs', 
