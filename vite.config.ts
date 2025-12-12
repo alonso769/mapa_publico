566d542e56d3eb5b601a4a23704e224de598acd5
alonso769 <131563220+alonso769@users.noreply.github.com>
2025-12-12 15:22:10 -0500

    cambio

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Determina la URL base automáticamente
+// Si estamos en VERCEL, usa '/'. Si no (como en GitHub Pages), usa el nombre del repositorio.
+const isVercel = process.env.VERCEL_ENV;
+const BASE_URL = isVercel ? '/' : '/mapa_publico/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   
-  // CAMBIO NECESARIO PARA GITHUB PAGES: Usa el nombre del repositorio
-  base: '/mapa_publico/', 
+  // Usamos la variable dinámica que cambia según el entorno
+  base: BASE_URL, 
 
   build: {
     outDir: 'docs', 
